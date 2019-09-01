3b3b04bc9c94b92bf5bb76a448886fe13e638e18
Joelle Helm <joellenhelm@gmail.com>
2019-09-01 17:13:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -58,7 +58,8 @@ def checkout(cart, coupons)
   consolidated = consolidate_cart(cart)
   coupons_applied = apply_coupons(consolidated, coupons)
   cleared = apply_clearance(coupons_applied)
-  cleared.each do |k, v|
+  consolidate_cleared = consolidate_cart(cleared)
+  consolidate_cleared.each do |k, v|
     total += cleared[k][:price]
   end
   if total >= 100
