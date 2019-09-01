0614fa46220647fe8fb7c2721b607c3173efe903
Joelle Helm <joellenhelm@gmail.com>
2019-09-01 18:19:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -31,7 +31,7 @@ def apply_coupons(cart, coupons)
         applied["#{item} W/COUPON"] = {:price => coupon[:cost] / coupon_num,
           :clearance => cart[item][:clearance],
           :count => coupon_num }
-          applied[item][:count] -= coupon_num
+          applied[item][:count] -= 1
       end
 
   end
